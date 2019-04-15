ed3e0ec7090567d8254e20f56fcb7b7f6fd40ab1
malbasi <15983663+malbasi@users.noreply.github.com>
2019-04-15 15:28:53 -0400

    final copy

--- dist/chart-4.9c4d6874.js
+++ dist/chart-4.9c4d6874.js
@@ -38655,7 +38655,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53205" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57305" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
